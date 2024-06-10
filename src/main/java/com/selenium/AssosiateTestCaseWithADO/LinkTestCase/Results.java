6768fcd55b9b36f3e059f1e7136e5316de0d6389
Vivek Singh bhatnagar <mail2vivek.bhatnagar@gmail.com>
2024-06-10 14:39:01 +0530

    Update Results.java

--- src/main/java/com/selenium/AssosiateTestCaseWithADO/LinkTestCase/Results.java
+++ src/main/java/com/selenium/AssosiateTestCaseWithADO/LinkTestCase/Results.java
@@ -1,5 +1,11 @@
 package com.selenium.AssosiateTestCaseWithADO.LinkTestCase;
-
+/**
+ * This File will define required paratmeter helpful in sending patch to ADO[Azure Devops]
+ ** @author Vivek Singh Bhatnagar
+ * @date 09 june 2024
+ * current working as Principal Software Engineer[QC] in Powerserv Technologies
+ * https://github.com/keviv72/
+ */
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Results {
